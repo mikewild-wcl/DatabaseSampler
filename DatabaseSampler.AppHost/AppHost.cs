3cab78caf5b2096f9d812b01bb0b70cc810438a4
Mike Wild <wildconsultingltd@googlemail.com>
2026-01-23 15:07:12 +0000

    Update DB emulators for persistence and new Cosmos options
    Switched Cosmos DB to use RunAsPreviewEmulator with Data Explorer, data volume, and persistent lifetime. Added pragma to suppress evaluation warning. Enabled data volume and persistent lifetime for Postgres and SQL Server resources to ensure data is retained across restarts.

--- DatabaseSampler.AppHost/AppHost.cs
+++ DatabaseSampler.AppHost/AppHost.cs
@@ -45,17 +45,26 @@ Links
 //var username = builder.AddParameter("username", secret: true);
 //var password = builder.AddParameter("password", secret: true);
 
+#pragma warning disable ASPIRECOSMOSDB001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 var cosmos = builder.AddAzureCosmosDB(ResourceNames.CosmosDB)
-  .RunAsEmulator();
+  .RunAsPreviewEmulator(emulator =>
+  {
+      emulator.WithDataExplorer();
+      emulator.WithDataVolume();
+      emulator.WithLifetime(ContainerLifetime.Persistent);
+  });
+#pragma warning restore ASPIRECOSMOSDB001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
 var postgresdb = builder.AddPostgres(ResourceNames.Postgres)
+    .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent)
     .AddDatabase(ResourceNames.PostgresDB, DatabaseNames.Students);
 
 var sqldb = builder.AddSqlServer(ResourceNames.SqlServer)
     //.WithImage("mssql/server", "2025-latest")
-    //.WithDataVolume()
+    .WithDataVolume()
     //.WithPassword(sqlPasswordParameter)
-    //.WithLifetime(ContainerLifetime.Persistent)
+    .WithLifetime(ContainerLifetime.Persistent)
     .AddDatabase(ResourceNames.SqlDatabase, DatabaseNames.Locations);
 
 var redis = builder.AddRedis(ResourceNames.Redis);
